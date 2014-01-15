ca8258a0a3ea653a90c2c4321c51a6a2db88a0d5
Steven Thuriot <steven@thuriot.be>
2014-01-15 18:24:52 +0100

    using webclient seems to work out better.

--- Powershell/noip.ps1
+++ Powershell/noip.ps1
@@ -1,12 +1,18 @@
-$hostname = "****"
-$pass = "****"
-$username = "****"
-
-
-$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
-$creds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+$hostname = "***.no-ip.org"
+$username = "***"
+$pass = "***"
 
 $ip = (Invoke-WebRequest -Uri "http://icanhazip.com").Content
+
+$dummyUri = "http://dynupdate.no-ip.com/nic/update?hostname=" + $hostname + "&myip=1.2.3.4"
 $noipUri = "http://dynupdate.no-ip.com/nic/update?hostname=" + $hostname + "&myip=" + $ip
 
-Invoke-WebRequest -Uri $noipUri -Credential $creds
+$webclient = new-object System.Net.WebClient
+$webclient.Credentials = new-object System.Net.NetworkCredential($username, $pass)
+
+
+$webclient.DownloadString($dummyUri)
+
+Start-Sleep -s 120
+
+$webclient.DownloadString($noipUri)
