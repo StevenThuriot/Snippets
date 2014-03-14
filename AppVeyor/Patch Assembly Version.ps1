5d20c850a5e5652926a8e51a89ac4896e34aab87
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2014-03-14 09:52:34 +0100

    Improved patch script

--- AppVeyor/Patch Assembly Version.ps1
+++ AppVeyor/Patch Assembly Version.ps1
@@ -5,20 +5,32 @@ $hasAssemblyVersion = "'"+$globalAssemblyVersion+"'" -match 'AssemblyVersion\("[
 
 if (!$hasAssemblyVersion)
 {
-	Add-AppveyorMessage -Message "No AssemblyVersion found."
-	return;
+	Add-AppveyorMessage -Message "No AssemblyVersion found, using 1.0.0.0 instead."
+	
+	$major=1
+	$minor=0
+	$build=0
+	$revision=0
 }
-
-$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9])+(\.(?<build>([0-9])))?(\.(?<revision>([0-9])))?"
-
-$major=$matches['major'] -as [int]
-$minor=$matches['minor'] -as [int]
-$build=$matches['build'] -as [int]
-$revision=$matches['revision'] -as [int]
+else
+{
+	$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9])+(\.(?<build>([0-9])))?(\.(?<revision>([0-9])))?"
+	
+	if (!$assemblyVersionFormattedCorrectly) 
+	{
+		Add-AppveyorMessage -Message "The Global Assembly Version is not formatted correctly."
+		return;
+	}
+	
+	$major=$matches['major'] -as [int]
+	$minor=$matches['minor'] -as [int]
+	$build=$matches['build'] -as [int]
+	$revision=$matches['revision'] -as [int]
+}
 
 $AssemblyVersion = "$major.$minor.$build.$revision"
 
-Add-AppveyorMessage -Message "Assembly Version: $AssemblyVersion"
+Add-AppveyorMessage -Message "Global Assembly Version: $AssemblyVersion ."
 
 $AssemblyFileVersion = "$major.$minor.$env:APPVEYOR_BUILD_NUMBER"
 $AssemblyInformationalVersion = "$AssemblyFileVersion-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
@@ -39,8 +51,41 @@ foreach( $file in $foundFiles )
 	}
 
 	$content = Get-Content "$file"
-		
+	
 	Add-AppveyorMessage -Message "Patching $file"
+	
+	$afv = $fileVersion
+	$aiv = $informationalVersion
+	
+	$hasFileAssemblyVersion = "'"+$content+"'" -match 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+
+	if ($hasFileAssemblyVersion)
+	{
+		$assemblyVersionFormattedCorrectly = $matches[0] -match "(?<major>[0-9]+)\.(?<minor>[0-9])+(\.(?<build>([0-9])))?(\.(?<revision>([0-9])))?"
+
+		if ($assemblyVersionFormattedCorrectly) 
+		{
+			$fileMajor=$matches['major'] -as [int]
+			$fileMinor=$matches['minor'] -as [int]	
+			$fileBuild=$matches['build'] -as [int]
+			$fileRevision=$matches['revision'] -as [int]	
+			
+			
+			$afv = "$fileMajor.$fileMinor.$env:APPVEYOR_BUILD_NUMBER"
+			$aiv = "$fileMajor.$fileMinor.-$env:APPVEYOR_REPO_SCM" + ($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
+			
+			Add-AppveyorMessage -Message "•	Specific AssemblyVersion found, using that instead: $fileMajor.$fileMinor.$fileBuild.$fileRevision ."
+			Add-AppveyorMessage -Message "	○	Patched File Version: $afv"
+			Add-AppveyorMessage -Message "	○	Patched Informational Version: $aiv"
+			
+			$afv = 'AssemblyFileVersion("' + $afv + '")';
+			$aiv = 'AssemblyInformationalVersion("' + $aiv + '")';
+		}
+		else
+		{
+			Add-AppveyorMessage -Message "• Specific AssemblyVersion found, but it's not formatted correctly, skipping."
+		}
+	}
 	
 	$content -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $fileVersion `
 			 -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $informationalVersion | Set-Content "$file"
