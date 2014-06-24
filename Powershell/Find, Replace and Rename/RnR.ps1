05f35d1471d7f41e9d778124b1129acb37bbedfd
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2014-06-24 09:25:39 +0200

    Less is more

--- Powershell/Find, Replace and Rename/RnR.ps1
+++ Powershell/Find, Replace and Rename/RnR.ps1
@@ -1,8 +1,4 @@
 param($old,$new)
 
-Get-ChildItem -recurse | Where-Object{$_.PSIsContainer -eq $false} | %{ $f=$_; (gc $f.PSPath) | %{ $_ -replace $old, $new } | sc $f.PSPath }	
-
-$items = gci -recurse -filter *$old*
-
-$items | Where-Object{$_.PSIsContainer -eq $false} | rename-item -newname {  $_.name  -replace $old,$new  }
-$items | Where-Object{$_.PSIsContainer} | rename-item -newname {  $_.name  -replace $old,$new  }
+Get-ChildItem -recurse | Where-Object{$_.PSIsContainer -eq $false} | %{ $f=$_; (Get-Content $f.PSPath) | %{ $_ -replace $old, $new } | Set-Content $f.PSPath }	
+Get-ChildItem -recurse -filter *$old* | Sort-Object -Property PSIsContainer | Rename-Item -NewName {  $_.Name  -replace $old, $new }
