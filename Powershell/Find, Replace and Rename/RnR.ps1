715bda8e6a13209aae9aea6af1c7c08b95b8bef2
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2014-06-25 15:28:53 +0200

    Leave images alone as well!

--- Powershell/Find, Replace and Rename/RnR.ps1
+++ Powershell/Find, Replace and Rename/RnR.ps1
@@ -1,4 +1,4 @@
 param($old,$new)
 
-Get-ChildItem -recurse -file -exclude *.pdb,*.dll,*.exe | %{ $f=$_; (Get-Content $f.PSPath) | %{ $_ -replace $old, $new } | Set-Content $f.PSPath }	
+Get-ChildItem -recurse -file -exclude *.pdb,*.dll,*.exe,*.jpg,*.jpeg,*.gif,*.png,*.bmp | %{ $f=$_; (Get-Content $f.PSPath) | %{ $_ -replace $old, $new } | Set-Content $f.PSPath }	
 Get-ChildItem -recurse -filter *$old* | Sort-Object -Property PSIsContainer | Rename-Item -NewName {  $_.Name  -replace $old, $new }
