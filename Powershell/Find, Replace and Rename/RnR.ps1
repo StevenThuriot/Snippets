22403e61b509ae10789adbeca368711cd49c5d57
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2014-06-25 15:23:00 +0200

    Optimized gci call

--- Powershell/Find, Replace and Rename/RnR.ps1
+++ Powershell/Find, Replace and Rename/RnR.ps1
@@ -1,4 +1,4 @@
 param($old,$new)
 
-Get-ChildItem -recurse | Where-Object{$_.PSIsContainer -eq $false} | %{ $f=$_; (Get-Content $f.PSPath) | %{ $_ -replace $old, $new } | Set-Content $f.PSPath }	
+Get-ChildItem -recurse -file -exclude *.pdb,*.dll,*.exe | %{ $f=$_; (Get-Content $f.PSPath) | %{ $_ -replace $old, $new } | Set-Content $f.PSPath }	
 Get-ChildItem -recurse -filter *$old* | Sort-Object -Property PSIsContainer | Rename-Item -NewName {  $_.Name  -replace $old, $new }
