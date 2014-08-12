203949dcd9671239ed0d4998f3d29d0c9f94aab4
Steven Thuriot <steven@thuriot.be>
2014-08-12 23:38:44 +0200

    updated git commit description

--- Scripts/PurgeHistory.cmd
+++ Scripts/PurgeHistory.cmd
@@ -2,7 +2,7 @@ rmdir /s /q .git
 
 git init
 git add .
-git commit -m "CDN"
+git commit -m "Initial Commit"
 
 git remote add origin https://github.com/thuriot/thuriot.github.io.git
 git push -u --force origin master
